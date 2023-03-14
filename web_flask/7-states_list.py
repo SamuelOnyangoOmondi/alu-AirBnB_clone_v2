63857de591e7e0b9da12932204130480d3412fce
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-03-14 22:00:05 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,75 +1,34 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
-import json
-from models.base_model import BaseModel
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes: - /states_list """
+from models import *
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from flask import Flask, render_template
+app = Flask(__name__)
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ display HTML page with list of states """
+    states = storage.all(classes["State"]).values()
+    # ^ fetches States data from storage engine, then in line below,
+    # those states are passed into the template
+    return render_template('7-states_list.html', states=states)
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if not cls:
-            return FileStorage.__objects
 
-        class_name = cls.__name__
-        objs = {}
-        for k, v in FileStorage.__objects.items():
-            if class_name in k:
-                objs[k] = v
+@app.teardown_appcontext
+def remove_SQLalc_session(exception):
+    """ close storage when tear down is executed """
+    storage.close()
 
-        return objs
-
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
-
-    def delete(self, obj=None):
-        """Delete obj from __objects if it’s inside"""
-        if not obj:
-            return
-
-        for k, v in FileStorage.__objects.items():
-            if v == obj:
-                del FileStorage.__objects[k]
-                return
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-
-    def close(self):
-        """
-        method for deserializing the JSON file to objects
-        """
-        self.reload()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
